d5450fee96cd436e8268fa55ee148695aa734511
Mayur Khodre <mayurkhodre2000@gmail.com>
2024-05-22 01:39:56 +0530

    MODIFY::MAYUR:HANDLED OPTIONAL COVER IMAGE UPLOAD AND COMMENTED FS.UNLINK CODE

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         });
         // file has been uploaded successfully
         console.log("file is uploaded on cloudinary ", response.url);
-        fs.unlink(localFilePath);
+        // fs.unlink(localFilePath);    // Need to fix this issue
         return response;
     } catch (error) {
         // remove the locally saved temporary file as the upload operation got failed
